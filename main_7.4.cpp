200225b6cbcb866d7cc61817c7d19f9917868984
Alex <astarso18@gmail.com>
2023-03-31 12:49:32 +0300

    Update main_7.4.cpp
    Fix

--- main_7.4.cpp
+++ main_7.4.cpp
@@ -1,51 +1,48 @@
 #include <stdio.h>
-
-// https://github.com/astarso/Programming-Technology-Starukhin-A.A./tree/Homework-7.4
+#include <string.h>
 
 // Функция, которая изменяет первый и последний символ в строке на заданный символ
 char* changeFirstLastChar(char *str, char ch) {
-    int len = 0;
+    int len = strlen(str);
 
-    // вычисляем длину строки
-    while(str[len] != '\0') {
-        len++;
-    }
+    // создаем копию строки
+    char* new_str = new char[len + 1];
+    strcpy(new_str, str);
 
     // если длина строки больше 1, изменяем первый и последний символ
     if (len > 1) {
-        str[0] = ch;
-        str[len - 1] = ch;
+        new_str[0] = ch;
+        new_str[len - 1] = ch;
     }
 
     // возвращаем указатель на измененную строку
-    return str;
+    return new_str;
 }
 
 // Функция, которая удаляет начальные и конечные пробелы в строке
 char* trimString(char *str) {
-    int len = 0;
+    int len = strlen(str);
 
-    // вычисляем длину строки
-    while(str[len] != '\0') {
-        len++;
-    }
+    // создаем копию строки
+    char* new_str = new char[len + 1];
+    strcpy(new_str, str);
 
     // удаляем начальные пробелы
-    while (str[0] == ' ' && len > 0) {
+    while (new_str[0] == ' ' && len > 0) {
         for (int i = 0; i < len; i++) {
-            str[i] = str[i+1];
+            new_str[i] = new_str[i+1];
         }
         len--;
     }
 
     // удаляем конечные пробелы
-    while (str[len-1] == ' ' && len > 0) {
-        str[len-1] = '\0';
+    while (new_str[len-1] == ' ' && len > 0) {
+        new_str[len-1] = '\0';
         len--;
     }
 
     // возвращаем указатель на измененную строку
-    return str;
+    return new_str;
 }
 
 int main() {
@@ -63,12 +60,14 @@ int main() {
     // изменяем первый и последний символ строки и выводим результат
     char *result1 = changeFirstLastChar(str, ch);
     printf("Original string: %s\nModified string: %s\n", str, result1);
+    delete[] result1; // освобождаем память, выделенную для копии строки
 
     // вводим новую строку и удаляем начальные и конечные пробелы
     printf("Enter a string: ");
     fgets(str, 100, stdin);
     char *result2 = trimString(str);
     printf("Original string: %s\nTrimmed string: %s\n", str, result2);
+    delete[] result2; // освобождаем память, выделенную для копии строки
 
     return 0;
 }
